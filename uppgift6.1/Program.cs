bb364e484816c9ad4480e57279364dab1b6496ad
en301 <emilsjostrand1000@gmail.com>
2024-02-14 10:31:49 +0100

    Add project files.

---
+++ uppgift6.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace uppgift_6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ut tre tal du vill addera");
+            int tal1 = int.Parse(Console.ReadLine());
+            int tal2 = int.Parse(Console.ReadLine());
+            int tal3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Summan av talen blir "+ Addera(tal1, tal2, tal3));
+            Console.ReadKey();
+        }
+        static int Addera(int tal1, int tal2, int tal3) 
+        {
+            return tal1 + tal2 + tal3;
+        }
+    }
+}
